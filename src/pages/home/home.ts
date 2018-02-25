7506b85cbe0033b39c8544cf6ffc0e2d099a3dfd
a0fzide <a0fzide@gmail.com>
2018-02-25 18:51:29 +0700

    add phaser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import Phaser from 'phaser'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,20 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    console.log(Phaser)
+    this.buildPhaserRenderer();
   }
 
+  private buildPhaserRenderer() {
+    var poly, graphics, game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create });
+    
+    function create() {
+        poly = new Phaser.Polygon();
+        poly.setTo([ new Phaser.Point(200, 100), new Phaser.Point(350, 100), new Phaser.Point(375, 200), new Phaser.Point(150, 200) ]);
+        graphics = game.add.graphics(0, 0);
+        graphics.beginFill(0xFF33ff);
+        graphics.drawPolygon(poly.points);
+        graphics.endFill();
+    }
+  }
 }
